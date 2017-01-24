115cea077c2e231a0dfe1d096b95efd653ac6c66
unicornt <unicornt@users.noreply.github.com>
2017-01-24 16:43:51 +0800

    Create template(short).cpp

---
+++ template(short).cpp
@@ -0,0 +1,33 @@
+#include <ctime>
+#include <cstdio>
+#include <vector>
+#include <cstring>
+#include <cassert>
+#include <iostream>
+#include <algorithm>
+#define fi first
+#define se second
+#define pb(x) push_back(x)
+#define clr(x,y) memset(x,y,sizeof(x))
+#define debug(x) cout<<#x<<" = "<<x<<endl;
+#define dig(...) fprintf(stderr,__VA_ARGS__)
+#define rep(i,a,n) for(int i=a,i##_END_=n;i<i##_END_;++i)
+#define per(i,a,n) for(int i=(n)-1,i##_BEGIN_=a;i>=i##_BEGIN_;--i)
+#define showtime fprintf(stderr,"time = %.15f\n",clock()/(double)CLOCKS_PER_SEC)
+using namespace std;
+typedef double db;
+typedef long long ll;
+inline void rd(int&res)
+{
+	res=0;char c;
+	bool flag=false;
+	while(c=getchar(),c!='-'&&c<'0'&&c>'9');
+	if(c=='-') flag=true,c=getchar();
+	do res=res*10+(c^48);
+	while(c=getchar(),c>='0'&&c<='9');
+	if(flag) res=-res;
+}
+int main()
+{
+	return 0;
+}
